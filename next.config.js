c1f6852b2704f2bff1cd1539ff9208934b63d9f3
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-05-08 23:43:29 +0800

    chore: modify env settings and rm unused file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
+const { NODE_ENV, __NEXT_PROCESSED_ENV, ...env } = process.env;
+
 module.exports = {
   images: {
-    domains: ["i.ytimg.com"],
+    domains: ["i.ytimg.com", "www.youtube.com"],
   },
-  env: {
-    YT_API_KEY: process.env.TY_API_KEY,
-  },
+  env,
 };
