e801246f34bca4e55ca87229a7da87b71326693f
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-05-08 23:51:12 +0800

    feat: add embed video & change styles

--- components/Video.js
+++ components/Video.js
@@ -4,21 +4,23 @@ import Link from "next/link";
 
 function Video({ video }) {
   const { id, snippet } = video;
-  console.log(video);
+  // console.log(video);
   return (
-    <figure className="mb-3 h-52 ">
-      <div className="flex flex-col items-center px-8 py-4 bg-black cursor-pointer">
-        <Link href="/watch/[id]" as={`/watch/${id}`}>
+    <div className="mb-3 h-52 ">
+      <Link href="/watch/[id]" as={`/watch/${id}`}>
+        <div className="flex flex-col items-center px-8 py-4 bg-black cursor-pointer">
           <Image src={snippet.thumbnails.high.url} width="auto" height="auto" />
-        </Link>
-      </div>
-      <figcaption
-        title={snippet.title}
-        className="h-auto mt-1 overflow-hidden text-center text-white cursor-pointer overflow-ellipsis whitespace-nowrap"
-      >
-        {snippet.title}
-      </figcaption>
-    </figure>
+        </div>
+      </Link>
+      <Link href="/watch/[id]" as={`/watch/${id}`}>
+        <div
+          title={snippet.title}
+          className="h-auto mt-1 overflow-hidden text-center text-white cursor-pointer overflow-ellipsis whitespace-nowrap"
+        >
+          {snippet.title}
+        </div>
+      </Link>
+    </div>
   );
 }
 
--- pages/watch/[id].js
+++ pages/watch/[id].js
@@ -3,7 +3,20 @@ import { useRouter } from "next/router";
 function watch() {
   const router = useRouter();
   const { id } = router.query;
-  return <div className="min-h-screen text-white">Video Id : {id}</div>;
+  
+  return (
+    <div className="flex justify-start min-h-screen pt-10 pl-3 text-white">
+      <iframe
+        src={`https://www.youtube.com/embed/${id}`}
+        width="75%"
+        height="450px"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
 }
 
 export default watch;
