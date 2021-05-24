5c22d53e90475ba864d5acea1c1c764f23f8a255
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-05-24 23:44:27 +0800

    feat: chage bg color and Navbar style

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,8 @@ import { ImFilm } from "react-icons/im";
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-center w-full h-12 text-white bg-codGray-900">
-      <div className="flex items-center justify-between w-full px-3 max-w-[980px]">
+    <nav className="flex items-center justify-center text-white h-14 bg-codGray-900">
+      <div className="flex items-center justify-between w-full px-6">
         <div className="flex items-center space-x-2 cursor-pointer">
           <ImFilm className="text-xl text-red-700" />
           <h1>Film Farm</h1>
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-codGray-500">
+    <div className="bg-codGray-400">
       <Navbar />
       <Component {...pageProps} />
     </div>
