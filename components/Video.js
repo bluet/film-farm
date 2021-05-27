696523ffcd020c60d8f8dadd3eb08466b312a3ed
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2021-05-27 23:57:28 +0800

    feat: modify video thumbnail size

--- components/Video.js
+++ components/Video.js
@@ -8,8 +8,13 @@ function Video({ video }) {
   return (
     <div className="mb-3 h-52">
       <Link href="/watch/[id]" as={`/watch/${id}`}>
-        <div className="flex flex-col items-center px-8 py-4 bg-black cursor-pointer">
-          <Image src={snippet.thumbnails.high.url} width="auto" height="auto" />
+        <div className="flex flex-col items-center px-4 py-2 bg-black cursor-pointer">
+          <Image
+            src={snippet.thumbnails.high.url}
+            width="auto"
+            height="auto"
+            objectFit="contain"
+          />
         </div>
       </Link>
       <Link href="/watch/[id]" as={`/watch/${id}`}>
