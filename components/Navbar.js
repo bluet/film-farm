ef372b5dd5df7529f119184e34c3b62c2805fa45
CJ-Yang0225 <57297482+CJ-Yang0225@users.noreply.github.com>
2021-05-27 23:55:44 +0800

    feat: add home page redirection and page title

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,17 +2,17 @@ import { ImFilm } from "react-icons/im";
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-center text-white h-14 bg-codGray-900">
-      <div className="flex items-center justify-between w-full px-6">
-        <div className="flex items-center space-x-2 cursor-pointer">
-          <ImFilm className="text-xl text-red-700" />
-          <h1>Film Farm</h1>
-        </div>
+    <nav className="flex items-center justify-center h-12 text-white bg-codGray-900">
+      <div className="pl-2 w-[1220px]">
+        <a href="/">
+          <div className="flex items-center space-x-2 cursor-pointer">
+            <ImFilm className="text-xl text-red-700" />
+            <h1>Film Farm</h1>
+          </div>
+        </a>
       </div>
     </nav>
   );
 }
-
-Navbar.propTypes = {};
 
 export default Navbar;
