26f8223f4db91f4ac47639b5880d7980f28a369a
ChaoJie Yang <57297482+CJ-Yang0225@users.noreply.github.com>
2021-06-02 17:28:21 +0800

    chore: modify tailwind structure

--- components/Ads/Floating.js
+++ components/Ads/Floating.js
@@ -1,7 +1,7 @@
 function Floating() {
   return (
     <div className="sticky top-0 hidden mx-1 text-2xl text-center bg-yellow-300 cursor-pointer xl:block w-[120px] h-[600px]">
-      <div className="relative top-[40%]">Floating Ads - 120x600</div>
+      Floating Ads - 120x600
     </div>
   );
 }
