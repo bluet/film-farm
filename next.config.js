cc5f9e343e0c7d14700f99c091e6d3379e00072f
ChaoJie Yang <57297482+CJ-Yang0225@users.noreply.github.com>
2021-06-24 15:38:40 +0800

    test deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const { NODE_ENV, __NEXT_PROCESSED_ENV, ...env } = process.env;
+// const { NODE_ENV, __NEXT_PROCESSED_ENV, ...env } = process.env;
 
 module.exports = {
   images: {
     domains: ["i.ytimg.com", "www.youtube.com"],
   },
-  env,
 };
