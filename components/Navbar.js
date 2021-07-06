2d0fab9e214435f9c743727dfaa5296ecd66d6ca
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-07-06 20:16:38 +0800

    fix: modify the home page icon click range

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="flex items-center justify-center h-12 text-white bg-codGray-900">
       <div className="pl-2 w-[1220px]">
         <a href="/">
-          <div className="flex items-center space-x-2 cursor-pointer">
+          <div className="inline-flex items-center space-x-2 cursor-pointer">
             <ImFilm className="text-xl text-red-700" />
             <h1>Film Farm</h1>
           </div>
